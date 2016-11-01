4eb1af7319290c4226e18d52759b4065ab437160
torresmatt <torresmatt@gmail.com>
2016-11-01 08:45:36 -0500

    add comments and cleaned up formatting on Mover.cs

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -8,30 +8,38 @@ public class Mover : MonoBehaviour {
     public float maxVelocity;
     public Rigidbody rb;
     public float dist;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+		
 	// Update is called once per frame
-	void Update () {
-        Vector3 mousePos = Input.mousePosition;
-        Vector3 cubePos = camera.WorldToScreenPoint(transform.position);
+	void Update () 
+	{
+		// store mouse position in terms of screen cooordinates
+        	Vector3 mousePos = Input.mousePosition;
+		
+		// store this transform (the cube's) in terms of screen coordinates
+       		Vector3 cubePos = camera.WorldToScreenPoint(transform.position);
+		
+		// store a vector representing the vector between the mouse and the transform of this object (cube)		
+        	Vector3 force = mousePos - cubePos;
+		
+		// store the distance between the mouse and the cube
+        	dist = Vector3.Distance(mousePos, cubePos);
+		
+		// reduce the magnitude of the force vector to whatever the maxForce variable is set to in the editor
+        	force = Vector3.ClampMagnitude(force, maxForce);
+		
+		// add the force to the cube
+        	rb.AddForce(force);
 
-        Vector3 force = mousePos - cubePos;
-        dist = Vector3.Distance(mousePos, cubePos);
-
-        force = Vector3.ClampMagnitude(force, maxForce);
-
-        rb.AddForce(force);
-
-        if (dist < 20)
-        {
-            rb.velocity = Vector3.ClampMagnitude(rb.velocity, dist / 100);
-            return;
-        }
-
-        rb.velocity = Vector3.ClampMagnitude(rb.velocity, dist / 10);
+		// is the cube closer than 20 pixels to the mouse?
+        	if (dist < 20)
+        	{
+			// clamp the velocity to a value determined by the distance between the mouse and the cube
+			// divide by 100 to make sure the velocity is very small (we dont want to go shooting past the pointer)
+            		rb.velocity = Vector3.ClampMagnitude(rb.velocity, dist / 100);
+            		return;
+        	}
+		
+		// clamp velocity by distance divided by 10 to reduce it slightly but not too much
+        	rb.velocity = Vector3.ClampMagnitude(rb.velocity, dist / 10);
 	}
 }
