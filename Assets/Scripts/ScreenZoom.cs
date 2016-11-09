cfa0a0e30cc05dd8cba178982f6dcabbde5509bd
torresmatt <torresmatt@gmail.com>
2016-11-08 22:10:16 -0600

    Can now zoom with scroll wheel!

---
+++ Assets/Scripts/ScreenZoom.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenZoom : MonoBehaviour {
+
+    private Camera camera;
+    private float scroll;
+
+    public float scrollSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+        camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll != 0)
+        {
+            camera.orthographicSize -= scroll * scrollSpeed;
+            camera.orthographicSize = Mathf.Clamp(camera.orthographicSize, 1, 50);
+        }
+	}
+}
